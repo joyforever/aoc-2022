430d8e1748761e026553acf460b0a8ef95543230
wangzixiang <wangzixiang@onething.net>
2022-12-02 22:30:57 +0800

    finish day 02

---
+++ day_02_rock_paper_scissors/src/lib.rs
@@ -0,0 +1,105 @@
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Shape {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+
+impl Shape {
+    fn parse(s: &str) -> Self {
+        if s == "A" || s == "X" {
+            Shape::Rock
+        } else if s == "B" || s == "Y" {
+            Shape::Paper
+        } else {
+            Shape::Scissors
+        }
+    }
+
+    fn score(&self) -> u32 {
+        *self as u32
+    }
+
+    fn compete(&self, opponent: Shape) -> u32 {
+        match (*self, opponent) {
+            (Shape::Rock, Shape::Rock) => 3,
+            (Shape::Rock, Shape::Paper) => 0,
+            (Shape::Rock, Shape::Scissors) => 6,
+            (Shape::Paper, Shape::Rock) => 6,
+            (Shape::Paper, Shape::Paper) => 3,
+            (Shape::Paper, Shape::Scissors) => 0,
+            (Shape::Scissors, Shape::Rock) => 0,
+            (Shape::Scissors, Shape::Paper) => 6,
+            (Shape::Scissors, Shape::Scissors) => 3,
+        }
+    }
+
+    fn choose(&self, guide: Guide) -> Shape {
+        match (*self, guide) {
+            (Shape::Rock, Guide::Lose) => Shape::Scissors,
+            (Shape::Rock, Guide::Win) => Shape::Paper,
+            (Shape::Paper, Guide::Lose) => Shape::Rock,
+            (Shape::Paper, Guide::Win) => Shape::Scissors,
+            (Shape::Scissors, Guide::Lose) => Shape::Paper,
+            (Shape::Scissors, Guide::Win) => Shape::Rock,
+            (shape, _) => shape,
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+enum Guide {
+    Lose,
+    Draw,
+    Win,
+}
+
+impl Guide {
+    fn parse(s: &str) -> Self {
+        if s == "X" {
+            Guide::Lose
+        } else if s == "Y" {
+            Guide::Draw
+        } else {
+            Guide::Win
+        }
+    }
+}
+
+pub fn part_one(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let (opponent, choose) = line.split_once(' ').unwrap();
+            let opponent = Shape::parse(opponent);
+            let choose = Shape::parse(choose);
+            choose.score() + choose.compete(opponent)
+        })
+        .sum()
+}
+
+pub fn part_two(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let (shape, guide) = line.split_once(' ').unwrap();
+            let shape = Shape::parse(shape);
+            let guide = Guide::parse(guide);
+            let choose = shape.choose(guide);
+            choose.score() + choose.compete(shape)
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = include_str!("../data/example.txt");
+
+    #[test]
+    fn it_works() {
+        assert_eq!(part_one(EXAMPLE), 15);
+        assert_eq!(part_two(EXAMPLE), 12);
+    }
+}
