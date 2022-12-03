3c11b4608178be83a06a939c99e4615e27790185
wangzixiang <wangzixiang@onething.net>
2022-12-03 19:42:34 +0800

    finish day 03 part two

--- day_03_rucksack_reorganization/src/lib.rs
+++ day_03_rucksack_reorganization/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 fn str_to_set(s: &str) -> HashSet<char> {
     s.chars().fold(HashSet::new(), |mut s, c| {
@@ -32,6 +32,40 @@ pub fn part_one(input: &str) -> u32 {
         .sum()
 }
 
+pub fn part_two(input: &str) -> u32 {
+    let lines: Vec<HashSet<char>> = input.lines().map(|line| str_to_set(line)).collect();
+
+    let mut priorities = vec![];
+
+    for i in (0..lines.len()).step_by(3) {
+        let r1 = &lines[i];
+        let r2 = &lines[i+1];
+        let r3 = &lines[i+2];
+
+        let mut m: HashMap<char, u32> = HashMap::new();
+        for c in r1 {
+            let e = m.entry(*c).or_default();
+            *e += 1;
+        }
+        for c in r2 {
+            let e = m.entry(*c).or_default();
+            *e += 1;
+        }
+        for c in r3 {
+            let e = m.entry(*c).or_default();
+            *e += 1;
+        }
+
+        if let Some((c, _)) = m.iter().filter(|(_, count)| **count == 3).next() {
+            priorities.push(char_to_priority(*c));
+        } else {
+            priorities.push(0);
+        }
+    }
+
+    priorities.iter().sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -41,5 +75,6 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(part_one(EXAMPLE), 157);
+        assert_eq!(part_two(EXAMPLE), 70);
     }
 }
--- day_03_rucksack_reorganization/src/main.rs
+++ day_03_rucksack_reorganization/src/main.rs
@@ -1,7 +1,8 @@
-use day_03_rucksack_reorganization::part_one;
+use day_03_rucksack_reorganization::{part_one, part_two};
 
 const INPUT: &str = include_str!("../data/input.txt");
 
 fn main() {
     println!("part one answer: {}", part_one(INPUT));
+    println!("part two answer: {}", part_two(INPUT));
 }
