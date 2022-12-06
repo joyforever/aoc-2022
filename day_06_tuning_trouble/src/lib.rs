a693dcf89c435b62a638780d1ff7ebaf89c85fc8
joyforever <joyforever@aliyun.com>
2022-12-06 13:32:16 +0800

    finish day 06 part two

--- day_06_tuning_trouble/src/lib.rs
+++ day_06_tuning_trouble/src/lib.rs
@@ -1,29 +1,45 @@
-pub fn part_one(input: &str) -> usize {
+use std::collections::HashSet;
+
+fn detect<const N: usize>(input: &str) -> usize {
     let b = input.as_bytes();
-    let n = b.len() - 4;
+    let n = b.len() - N;
     for i in 0..=n {
-        let all_different = 
-            b[i] != b[i+1] &&
-            b[i] != b[i+2] &&
-            b[i] != b[i+3] &&
-            b[i+1] != b[i+2] &&
-            b[i+1] != b[i+3] &&
-            b[i+2] != b[i+3];
-        if all_different {
-            return i + 4;
+        let s = (0..N)
+            .fold(HashSet::new(), |mut s, c| {
+                s.insert(b[i+c]);
+                s
+            });
+        if s.len() == N {
+            return i + N;
         }
     }
     0
+}
+
+pub fn part_one(input: &str) -> usize {
+    detect::<4>(input)
+}
+
+pub fn part_two(input: &str) -> usize {
+    detect::<14>(input)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = include_str!("../data/example.txt");
-
     #[test]
     fn it_works() {
-        assert_eq!(part_one(EXAMPLE), 7);
+        assert_eq!(part_one("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
+        assert_eq!(part_one("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+        assert_eq!(part_one("nppdvjthqldpwncqszvftbrmjlhg"), 6);
+        assert_eq!(part_one("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
+        assert_eq!(part_one("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+
+        assert_eq!(part_two("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
+        assert_eq!(part_two("bvwbjplbgvbhsrlpgdmjqwftvncz"), 23);
+        assert_eq!(part_two("nppdvjthqldpwncqszvftbrmjlhg"), 23);
+        assert_eq!(part_two("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29);
+        assert_eq!(part_two("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26);
     }
 }
