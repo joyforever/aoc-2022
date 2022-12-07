c55bab01d25b977ffb72ff38ca2595ad8a0d4ac4
wangzixiang <wangzixiang@onething.net>
2022-12-07 22:57:55 +0800

    finish day 07 part one

--- day_07_no_space_left_on_device/src/lib.rs
+++ day_07_no_space_left_on_device/src/lib.rs
@@ -1,13 +1,25 @@
+#![allow (dead_code)]
+
 #[derive(Debug)]
 enum Item {
     Dir {
         name: String,
+        size: usize,
         items: Vec<Item>,
     },
     File {
         name: String,
         size: usize,
     },
+}
+
+impl Item {
+    fn size(&self) -> usize {
+        match self {
+            Item::Dir { size, name: _, items: _, } => *size,
+            Item::File { size, name: _, } => *size,
+        }
+    }
 }
 
 fn parse_item(mut index: usize, lines: &[&str], dir: &mut Item) -> usize {
@@ -28,20 +40,23 @@ fn parse_item(mut index: usize, lines: &[&str], dir: &mut Item) -> usize {
 
             let mut dir_item = Item::Dir {
                 name: name.to_string(),
+                size: 0,
                 items: vec![],
             };
             index = parse_item(index + 1, lines, &mut dir_item);
-            if let Item::Dir { items, name: _ } = dir {
+            if let Item::Dir { name: _, size, items, } = dir {
+                *size += dir_item.size();
                 items.push(dir_item);
             }
         } else if line.starts_with("dir ") {
             index += 1;
         } else {
-            let (size, name) = line.split_once(' ').unwrap();
-            let size = size.parse::<usize>().unwrap();
-            let name = name.to_string();
-            if let Item::Dir { items, name: _ } = dir {
-                items.push(Item::File { name, size });
+            let (file_size, file_name) = line.split_once(' ').unwrap();
+            let file_size = file_size.parse::<usize>().unwrap();
+            let file_name = file_name.to_string();
+            if let Item::Dir { name: _, size, items, } = dir {
+                *size += file_size;
+                items.push(Item::File { name: file_name, size: file_size, });
             }
             index += 1;
         }
@@ -49,14 +64,26 @@ fn parse_item(mut index: usize, lines: &[&str], dir: &mut Item) -> usize {
     index
 }
 
+fn find_directories(item: &Item) -> usize {
+    let mut count = 0;
+    if let Item::Dir { name: _, size, items } = item {
+        if *size <= 100000 {
+            count += *size;
+        }
+        for sub_item in items {
+            count += find_directories(sub_item);
+        }
+    }
+    count
+}
+
 pub fn part_one(input: &str) -> usize {
     let lines = input.lines().skip(1).collect::<Vec<_>>();
-    let mut root = Item::Dir { name: "/".to_string(), items: vec![], };
+
+    let mut root = Item::Dir { name: "/".to_string(), size: 0, items: vec![], };
     parse_item(1, &lines, &mut root);
 
-    dbg!(root);
-
-    0
+    find_directories(&root)
 }
 
 #[cfg(test)]
