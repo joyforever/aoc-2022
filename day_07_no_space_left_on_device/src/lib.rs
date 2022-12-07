e37506db3585f8e447aac98008bf3b01af6c2dd6
joyforever <joyforever@aliyun.com>
2022-12-07 21:39:03 +0800

    list all files in dir

--- day_07_no_space_left_on_device/src/lib.rs
+++ day_07_no_space_left_on_device/src/lib.rs
@@ -1,5 +1,55 @@
-pub fn part_one(_input: &str) -> usize {
-    0
+use std::collections::BTreeMap;
+
+fn to_string(v: &Vec<String>) -> String {
+    if v.is_empty() {
+        "".to_string()
+    } else if v.len() == 1 {
+        "/".to_string()
+    } else {
+        let mut s = String::from("/");
+        s.push_str(v[1..].join("/").as_str());
+        s
+    }
+}
+
+pub fn part_one(input: &str) -> usize {
+    let mut files_map = BTreeMap::new();
+    let mut curr_path = Vec::new();
+    let mut files = Vec::new();
+
+    for line in input.lines() {
+        if line.starts_with("$ cd") {
+            if !files.is_empty() {
+                files_map.insert(to_string(&curr_path), files.clone());
+                files.clear();
+            }
+
+            let path = &line[5..];
+            if path == ".." {
+                curr_path.pop();
+            } else {
+                curr_path.push(path.to_string());
+            }
+        } else if line.starts_with("$ ls") {
+            files.clear();
+        } else if line.starts_with("dir ") {
+            // skip
+        } else {
+            let (size, name) = line.split_once(' ').unwrap();
+            let size = size.parse::<usize>().unwrap();
+            files.push((size, name.to_string()));
+        }
+    }
+
+    files_map
+        .iter()
+        .map(|(path, v)| {
+            let total = v.iter().fold(0, |total, (size, _)| total + *size);
+            println!("path={}, total={}", path, total);
+            total
+        })
+        .filter(|&size| size <= 100000)
+        .sum::<usize>()
 }
 
 #[cfg(test)]
