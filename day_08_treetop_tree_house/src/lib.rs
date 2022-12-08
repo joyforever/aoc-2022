fd486ffbdec8a24048a4cc0eb981205a2d6ab4b3
joyforever <joyforever@aliyun.com>
2022-12-08 13:38:35 +0800

    finish day 08 part one

--- day_08_treetop_tree_house/src/lib.rs
+++ day_08_treetop_tree_house/src/lib.rs
@@ -1,5 +1,29 @@
 pub fn part_one(input: &str) -> usize {
-    0
+    let map = input
+        .lines()
+        .map(|line| line.as_bytes().to_vec())
+        .collect::<Vec<_>>();
+    let rows = map.len();
+    let cols = map[0].len();
+
+    let edge_visible = rows * 2 + (cols - 2) * 2;
+    let mut interior_visible = 0;
+    for row in 1..rows-1 {
+        for col in 1..cols-1 {
+            let height = map[row][col];
+
+            let is_visiable = 
+                (0..row).all(|r| map[r][col] < height) ||
+                (row+1..rows).all(|r| map[r][col] < height) ||
+                (0..col).all(|c| map[row][c] < height) ||
+                (col+1..cols).all(|c| map[row][c] < height);
+            if is_visiable {
+                interior_visible += 1;
+            }
+        }
+    }
+
+    edge_visible + interior_visible
 }
 
 #[cfg(test)]
