a00155e90224c73fec2929497b99d7d45cfb6064
wangzixiang <wangzixiang@onething.net>
2022-12-09 17:32:10 +0800

    finish day 09 part two

--- day_09_rope_bridge/src/lib.rs
+++ day_09_rope_bridge/src/lib.rs
@@ -58,31 +58,22 @@ fn parse_motions(input: &str) -> Vec<Motion> {
         .collect()
 }
 
-fn move_tail(head: Position, tail: &mut Position, direction: Direction) -> Position {
+fn move_tail(head: Position, tail: &mut Position) {
     if head.x.abs_diff(tail.x) <= 1 && head.y.abs_diff(tail.y) <= 1 {
-        return *tail;
+        return;
     }
 
-    tail.step(direction);
-
-    match direction {
-        Direction::Left | Direction::Right => {
-            if tail.y > head.y && tail.y - head.y >= 1 {
-                tail.y -= 1;
-            } else if tail.y < head.y && head.y - tail.y >= 1 {
-                tail.y += 1;
-            }
-        },
-        Direction::Up | Direction::Down => {
-            if tail.x > head.x && tail.x - head.x >= 1 {
-                tail.x -= 1;
-            } else if tail.x < head.x && head.x - tail.x >= 1 {
-                tail.x += 1;
-            }
-        },
+    if tail.x > head.x && tail.x - head.x >= 1 {
+        tail.x -= 1;
+    } else if tail.x < head.x && head.x - tail.x >= 1 {
+        tail.x += 1;
     }
 
-    *tail
+    if tail.y > head.y && tail.y - head.y >= 1 {
+        tail.y -= 1;
+    } else if tail.y < head.y && head.y - tail.y >= 1 {
+        tail.y += 1;
+    }
 }
 
 pub fn part_one(input: &str) -> usize {
@@ -97,7 +88,8 @@ pub fn part_one(input: &str) -> usize {
     for motion in &motions {
         (0..motion.steps).for_each(|_| {
             head.step(motion.direction);
-            map.insert(move_tail(head, &mut tail, motion.direction));
+            move_tail(head, &mut tail);
+            map.insert(tail);
             //println!("{head:?} {tail:?}");
         });
     }
@@ -116,14 +108,39 @@ pub fn part_two(input: &str) -> usize {
         knots.push((pos, set));
     }
 
-    for motion in &motions {
+    for motion in motions.iter() {
+        //println!("== {:?} {} ==", motion.direction, motion.steps);
+
         (0..motion.steps).for_each(|_| {
             knots[0].0.step(motion.direction);
             for i in 1..=9 {
-                let tail = move_tail(knots[i-1].0, &mut knots[i].0, motion.direction);
+                move_tail(knots[i-1].0, &mut knots[i].0);
+                let tail = knots[i].0;
                 knots[i].1.insert(tail);
             }
         });
+
+        // for y in (-10..10).rev() {
+        //     for x in -10..10 {
+        //         let mut found = false;
+        //         for (i, k) in knots.iter().enumerate() {
+        //             if k.0.x == x && k.0.y == y {
+        //                 if i == 0 {
+        //                     print!("H");
+        //                 } else {
+        //                     print!("{i}");
+        //                 }
+        //                 found = true;
+        //                 break;
+        //             }
+        //         }
+        //         if !found {
+        //             print!(".");
+        //         }
+        //     }
+        //     println!();
+        // }
+        // println!();
     }
 
     knots[9].1.len()
@@ -134,6 +151,7 @@ mod tests {
     use super::*;
 
     const EXAMPLE: &str = include_str!("../data/example.txt");
+    const LARGE_EXAMPLE: &str = include_str!("../data/large_example.txt");
 
     #[test]
     fn part_one_works() {
@@ -143,5 +161,6 @@ mod tests {
     #[test]
     fn part_two_works() {
         assert_eq!(part_two(EXAMPLE), 1);
+        assert_eq!(part_two(LARGE_EXAMPLE), 36);
     }
 }
