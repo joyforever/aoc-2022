ab1f5c4bb1cf324a3cd1f41db85d1835946318e4
wangzixiang <wangzixiang@onething.net>
2022-12-13 01:29:06 +0800

    simpe impl for day 12

--- day_12_hill_climbing_algorithm/src/lib.rs
+++ day_12_hill_climbing_algorithm/src/lib.rs
@@ -1,121 +1,123 @@
-use petgraph::{algo::dijkstra, prelude::DiGraphMap};
+use petgraph::{
+    algo::dijkstra,
+    dot::{Config, Dot},
+    prelude::DiGraphMap,
+};
 
-struct HeightMap {
-    grid: Vec<Vec<char>>,
-    start: (usize, usize),
-    end: (usize, usize),
-    graph: DiGraphMap<(usize, usize), ()>,
-}
+struct Grid(Vec<Vec<char>>);
 
-impl From<&str> for HeightMap {
+impl From<&str> for Grid {
     fn from(s: &str) -> Self {
         let grid = s
             .lines()
             .map(|line| line.chars().collect::<Vec<_>>())
             .collect::<Vec<_>>();
-        let mut start = (0, 0);
-        let mut end = (0, 0);
+        Self(grid)
+    }
+}
 
-        let rows = grid.len();
-        let cols = grid[0].len();
+type Node = (usize, usize, char);
+
+impl Grid {
+    fn find_char(&self, ch: char) -> Option<Node> {
+        for (i, v) in self.0.iter().enumerate() {
+            for (j, c) in v.iter().enumerate() {
+                if *c == ch {
+                    return Some((i, j, ch));
+                }
+            }
+        }
+        None
+    }
+
+    fn to_edges(&self) -> Vec<(Node, Node)> {
+        let rows = self.0.len();
+        let cols = self.0[0].len();
 
         let mut edges = vec![];
         for i in 0..rows {
             for j in 0..cols {
-                let c = grid[i][j];
-
-                let c = match c {
-                    'S' => {
-                        start.0 = i;
-                        start.1 = j;
-                        'a'
-                    }
-                    'E' => {
-                        end.0 = i;
-                        end.1 = j;
-                        'z'
-                    }
-                    c => c,
-                };
-
                 let translate = |c| match c {
-                    'S' => 'a',
-                    'E' => 'z',
-                    c => c,
+                    'S' => (c, 'a'),
+                    'E' => (c, 'z'),
+                    c => (c, c),
                 };
+
+                // o: original
+                // m: mapped
+                let (oc, mc) = translate(self.0[i][j]);
 
                 // left
                 if j > 0 {
-                    let left_c = translate(grid[i][j - 1]);
-                    if left_c as i32 - c as i32 <= 1 {
-                        edges.push(((i, j), (i, j - 1)));
+                    let (left_oc, left_mc) = translate(self.0[i][j - 1]);
+                    if left_mc as i32 - mc as i32 <= 1 {
+                        edges.push(((i, j, oc), (i, j - 1, left_oc)));
                     }
                 }
                 // right
                 if j < cols - 1 {
-                    let right_c = translate(grid[i][j + 1]);
-                    if right_c as i32 - c as i32 <= 1 {
-                        edges.push(((i, j), (i, j + 1)));
+                    let (right_oc, right_mc) = translate(self.0[i][j + 1]);
+                    if right_mc as i32 - mc as i32 <= 1 {
+                        edges.push(((i, j, oc), (i, j + 1, right_oc)));
                     }
                 }
                 // up
                 if i > 0 {
-                    let up_c = translate(grid[i - 1][j]);
-                    if up_c as i32 - c as i32 <= 1 {
-                        edges.push(((i, j), (i - 1, j)));
+                    let (up_oc, up_mc) = translate(self.0[i - 1][j]);
+                    if up_mc as i32 - mc as i32 <= 1 {
+                        edges.push(((i, j, oc), (i - 1, j, up_oc)));
                     }
                 }
                 // down
                 if i < rows - 1 {
-                    let down_c = translate(grid[i + 1][j]);
-                    if down_c as i32 - c as i32 <= 1 {
-                        edges.push(((i, j), (i + 1, j)));
+                    let (down_oc, down_mc) = translate(self.0[i + 1][j]);
+                    if down_mc as i32 - mc as i32 <= 1 {
+                        edges.push(((i, j, oc), (i + 1, j, down_oc)));
                     }
                 }
             }
         }
 
-        let graph = DiGraphMap::<(usize, usize), ()>::from_edges(&edges);
-        //println!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
-
-        Self {
-            grid,
-            start,
-            end,
-            graph,
-        }
+        edges
     }
+}
+
+#[allow(dead_code)]
+fn print_graph(graph: DiGraphMap<Node, ()>) {
+    println!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
 }
 
 pub fn part_one(input: &str) -> usize {
-    let map = HeightMap::from(input);
-    let res = dijkstra(&map.graph, map.start, None, |_| 1usize);
+    let grid = Grid::from(input);
+    let start = grid.find_char('S').unwrap();
+    let end = grid.find_char('E').unwrap();
 
-    *res.get(&map.end).unwrap()
+    let edges = grid.to_edges();
+    let graph = DiGraphMap::<(usize, usize, char), ()>::from_edges(&edges);
+
+    let res = dijkstra(&graph, start, None, |_| 1usize);
+    *res.get(&end).unwrap()
 }
 
 pub fn part_two(input: &str) -> usize {
-    let map = HeightMap::from(input);
-    let grid = &map.grid;
+    let grid = Grid::from(input);
+    let end = grid.find_char('E').unwrap();
 
-    let rows = grid.len();
-    let cols = grid[0].len();
+    let edges = grid.to_edges();
+    let edges = edges.iter().map(|(v1, v2)| (*v2, *v1)).collect::<Vec<_>>();
+    let graph = DiGraphMap::<(usize, usize, char), ()>::from_edges(&edges);
 
-    let mut paths = vec![];
-    for i in 0..rows {
-        for j in 0..cols {
-            let c = grid[i][j];
-            if c == 'a' || c == 'S' {
-                let res = dijkstra(&map.graph, (i, j), None, |_| 1usize);
-                if let Some(v) = res.get(&map.end) {
-                    paths.push(*v);
-                }
+    let res = dijkstra(&graph, end, None, |_| 1usize);
+    res.iter()
+        .filter_map(|(index, value)| {
+            if index.2 == 'a' || index.2 == 'S' {
+                Some(*value)
+            } else {
+                None
             }
-        }
-    }
-
-    paths.sort();
-    paths[0]
+        })
+        .min()
+        .unwrap()
 }
 
 #[cfg(test)]
