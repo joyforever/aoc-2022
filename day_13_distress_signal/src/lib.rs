80e32212c87c1b9e1659e31caec4520eaaeacdfd
wangzixiang <wangzixiang@onething.net>
2022-12-13 13:27:24 +0800

    add day 13 input

---
+++ day_13_distress_signal/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn part_one(_input: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = include_str!("../data/example.txt");
+
+    #[test]
+    fn part_one_works() {
+        assert_eq!(part_one(EXAMPLE), 13);
+    }
+}
---
+++ day_13_distress_signal/src/main.rs
@@ -0,0 +1,7 @@
+use day_13_distress_signal::part_one;
+
+const INPUT: &str = include_str!("../data/input.txt");
+
+fn main() {
+    println!("part one answer: {}", part_one(INPUT));
+}
