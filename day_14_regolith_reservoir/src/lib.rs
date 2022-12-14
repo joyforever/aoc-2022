7dd726af546dc617b027eff262c2e3a83f4661c0
wangzixiang <wangzixiang@onething.net>
2022-12-15 01:55:20 +0800

    simplify code

--- day_14_regolith_reservoir/src/lib.rs
+++ day_14_regolith_reservoir/src/lib.rs
@@ -60,11 +60,19 @@ impl Cave {
     }
 
     fn add_rock(&mut self, x: i32, y: i32) {
+        self.add_unit(x, y, Unit::Rock);
+    }
+
+    fn add_sand(&mut self, x: i32, y: i32) {
+        self.add_unit(x, y, Unit::Sand);
+    }
+
+    fn add_unit(&mut self, x: i32, y: i32, unit: Unit) {
         self.min_x = self.min_x.min(x);
         self.max_x = self.max_x.max(x);
         self.min_y = self.min_y.min(y);
         self.max_y = self.max_y.max(y);
-        self.units.insert((x, y), Unit::Rock);
+        self.units.insert((x, y), unit);
     }
 
     fn is_out(&self, x: i32, y: i32) -> bool {
@@ -75,55 +83,31 @@ impl Cave {
         self.units.get(&(x, y)).is_none()
     }
 
-    fn add_sand(&mut self, x: i32, y: i32) {
-        self.min_x = self.min_x.min(x);
-        self.max_x = self.max_x.max(x);
-        self.min_y = self.min_y.min(y);
-        self.max_y = self.max_y.max(y);
-        self.units.insert((x, y), Unit::Sand);
-    }
-
-    fn fall_sand(&mut self) -> bool {
+    fn fall_sand(&mut self) -> Option<(i32, i32)> {
         let mut x = self.source.0;
         let mut y = self.source.1;
 
-        loop {
-            // down
-            if self.is_out(x, y + 1) {
-                return false;
-            }
-            if self.is_air(x, y + 1) {
-                y += 1;
-                continue;
-            }
-
-            // left down
-            if self.is_out(x - 1, y + 1) {
-                return false;
-            }
-            if self.is_air(x - 1, y + 1) {
-                x -= 1;
-                y += 1;
-                continue;
-            }
-
-            // right down
-            if self.is_out(x + 1, y + 1) {
-                return false;
-            }
-            if self.is_air(x + 1, y + 1) {
-                x += 1;
-                y += 1;
-                continue;
+        'outer: loop {
+            for d in [(0,1), (-1,1), (1,1)] {
+                if self.is_out(x + d.0, y + d.1) {
+                    return None;
+                }
+                if self.is_air(x + d.0, y + d.1) {
+                    x += d.0;
+                    y += d.1;
+                    continue 'outer;
+                }
             }
 
             if (x, y) == self.source {
+                // source has filled with sand
                 if !self.is_air(x, y) {
-                    return false;
+                    return None;
                 }
             }
-            self.add_sand(x, y);
-            return true;
+
+            // sand can rest here
+            return Some((x, y));
         }
     }
 }
@@ -160,7 +144,7 @@ fn parse_paths(input: &str) -> Vec<Vec<(i32, i32)>> {
         .map(|line| {
             line.split(" -> ")
                 .map(|s| s.split_once(',').unwrap())
-                .map(|(x, y)| (x.parse::<i32>().unwrap(), y.parse::<i32>().unwrap()))
+                .map(|(x, y)| (x.parse().unwrap(), y.parse().unwrap()))
                 .collect::<Vec<_>>()
         })
         .collect::<Vec<_>>()
@@ -173,7 +157,8 @@ pub fn part_one(input: &str) -> usize {
 
     //println!("{cave}\n");
     let mut count = 0;
-    while cave.fall_sand() {
+    while let Some(coord) = cave.fall_sand() {
+        cave.add_sand(coord.0, coord.1);
         count += 1;
         //println!("{cave}\n");
     }
@@ -191,7 +176,8 @@ pub fn part_two(input: &str) -> usize {
 
     //println!("{cave}\n");
     let mut count = 0;
-    while cave.fall_sand() {
+    while let Some(coord) = cave.fall_sand() {
+        cave.add_sand(coord.0, coord.1);
         count += 1;
         //println!("{cave}\n");
     }
