951bbe3a74a74915b4df3b785bb9f05b15ead7f7
wangzixiang <wangzixiang@onething.net>
2022-12-14 14:23:58 +0800

    parse input

--- day_14_regolith_reservoir/src/lib.rs
+++ day_14_regolith_reservoir/src/lib.rs
@@ -1,4 +1,15 @@
-pub fn part_one(_input: &str) -> usize {
+pub fn part_one(input: &str) -> usize {
+    let paths = input
+        .trim()
+        .lines()
+        .map(|line| line
+            .split(" -> ")
+            .map(|s| s.split_once(',').unwrap())
+            .map(|(x, y)| (x.parse::<u32>().unwrap(), y.parse::<u32>().unwrap()))
+            .collect::<Vec<_>>()
+        )
+        .collect::<Vec<_>>();
+    println!("{paths:?}");
     0
 }
 
@@ -9,7 +20,7 @@ mod tests {
     const EXAMPLE: &str = include_str!("../data/example.txt");
 
     #[test]
-    fn it_works() {
+    fn part_one_works() {
         assert_eq!(part_one(EXAMPLE), 0);
     }
 }
