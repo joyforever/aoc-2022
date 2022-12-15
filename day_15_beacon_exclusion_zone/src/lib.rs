a1a7e2e6872ed9665f0d50991f71f9ced93a2617
joyforever <joyforever@aliyun.com>
2022-12-15 20:31:57 +0800

    增加解析逻辑

--- day_15_beacon_exclusion_zone/src/lib.rs
+++ day_15_beacon_exclusion_zone/src/lib.rs
@@ -1,5 +1,52 @@
-pub fn part_one(_input: &str) -> usize {
-    0
+use std::collections::{HashMap, HashSet};
+
+fn parse_position(tag: &str, s: &str) -> (i32, i32) {
+    let n = tag.len();
+    let s = &s[n..];
+    let (x, y) = s.split_once(", ").unwrap();
+    let x = x[2..].parse::<i32>().unwrap();
+    let y = y[2..].parse::<i32>().unwrap();
+    (x, y)
+}
+
+pub fn part_one(input: &str) -> usize {
+    let pairs = input
+        .trim()
+        .lines()
+        .map(|line| {
+            let (s1, s2) = line.split_once(": ").unwrap();
+            let sensor = parse_position("Sensor at ", s1);
+            let beacon = parse_position("closest beacon is at ", s2);
+            (sensor, beacon)
+        })
+        .collect::<Vec<_>>();
+
+    let mut map = HashMap::new();    
+    for (sensor, beacon) in &pairs {
+        map.insert(sensor, 'S');
+        map.insert(beacon, 'B');
+    }
+
+    let mut set = HashSet::new();
+
+    for (sensor, beacon) in &pairs {
+        let n = sensor.0.abs_diff(beacon.0) as i32 + sensor.1.abs_diff(beacon.1) as i32;
+        for i in -n..=n {
+            for j in -(n-i)..=n-i {
+                let x = sensor.0 + i;
+                let y = sensor.1 + j;
+                if y == 10 {
+                    set.insert((x, y));
+                }
+            }
+        }
+    }
+
+    for s in &set {
+        println!("{s:?}");
+    }
+
+    set.len()
 }
 
 #[cfg(test)]
@@ -9,7 +56,7 @@ mod tests {
     const EXAMPLE: &str = include_str!("../data/example.txt");
 
     #[test]
-    fn it_works() {
+    fn part_one_works() {
         assert_eq!(part_one(EXAMPLE), 26);
     }
 }
