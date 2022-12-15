291dba216503ada3d828d56b09de1db02b536023
joyforever <joyforever@aliyun.com>
2022-12-15 21:11:24 +0800

    finish day 15 part one

--- day_15_beacon_exclusion_zone/src/main.rs
+++ day_15_beacon_exclusion_zone/src/main.rs
@@ -3,5 +3,5 @@ use day_15_beacon_exclusion_zone::part_one;
 const INPUT: &str = include_str!("../data/input.txt");
 
 fn main() {
-    println!("part one answer: {}", part_one(INPUT, 200000));
+    println!("part one answer: {}", part_one(INPUT, 2000000));
 }
